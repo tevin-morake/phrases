1c0d029c149f129fb29900d5de0da074a5f856ac
Tevin Morake <tevinm@runninghill.co.za>
2021-03-02 11:18:45 +0200

    added audio files, updated MainActivity to play each phrase on press

--- app/src/main/java/com/example/phrases/MainActivity.java
+++ app/src/main/java/com/example/phrases/MainActivity.java
@@ -1,14 +1,76 @@
 package com.example.phrases;
 
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    Button button1, button2, button3, button4, button5, button6, button7, button8;
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button1 = (Button) findViewById(R.id.button1);
+
+        button2 = (Button) findViewById(R.id.button2);
+
+        button3 = (Button) findViewById(R.id.button3);
+
+        button4 = (Button) findViewById(R.id.button4);
+
+        button5 = (Button) findViewById(R.id.button5);
+
+        button6 = (Button) findViewById(R.id.button6);
+
+        button7 = (Button) findViewById(R.id.button7);
+
+        button8 = (Button) findViewById(R.id.button8);
+
+    }
+
+    public void onButtonPress(View v) {
+        final int id = v.getId();
+
+
+        switch (id) {
+            case R.id.button1:
+                mediaPlayer = MediaPlayer.create(this, R.raw.doyouspeakenglish);
+                mediaPlayer.start();
+                break;
+            case R.id.button2:
+                mediaPlayer = MediaPlayer.create(this, R.raw.goodevening);
+                mediaPlayer.start();
+                break;
+            case R.id.button3:
+                mediaPlayer = MediaPlayer.create(this, R.raw.hello);
+                mediaPlayer.start();
+                break;
+            case R.id.button4:
+                mediaPlayer = MediaPlayer.create(this, R.raw.howareyou);
+                mediaPlayer.start();
+                break;
+            case R.id.button5:
+                mediaPlayer = MediaPlayer.create(this, R.raw.ilivein);
+                mediaPlayer.start();
+                break;
+            case R.id.button6:
+                mediaPlayer = MediaPlayer.create(this, R.raw.mynameis);
+                mediaPlayer.start();
+                break;
+            case R.id.button7:
+                mediaPlayer = MediaPlayer.create(this, R.raw.please);
+                mediaPlayer.start();
+                break;
+            case R.id.button8:
+                mediaPlayer = MediaPlayer.create(this, R.raw.welcome);
+                mediaPlayer.start();
+                break;
+        }
     }
 }
